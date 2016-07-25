2bc7c6cd97d2e9ccb5ec224c1b92bb5004cd0446
tpowell <tpowell@slac.stanford.edu>
2016-07-25 14:02:53 -0700

    Line scans across top 5 hybrid patters; Saturation threshold set

--- saturation_threshold.py
+++ saturation_threshold.py
@@ -58,7 +58,7 @@ for i in max_intensities:
     dex = np.where(intensities==i)
     top_hybridPatterns.append(Patterns[dex][0])
 
-print top_hybridPatterns
+#print top_hybridPatterns
 
 #------------------------------------------------------
 #Print information of interest (average/max intensity)
@@ -111,52 +111,93 @@ x1, y1 = 250, 133
 num = 1000
 x, y = np.linspace(x0, x1, num), np.linspace(y0, y1, num)
 
-n0, m0 = 0, 139 
-n1, m1 = 250, 139
+n0, m0 = 0, 138 
+n1, m1 = 250, 138
 n, m = np.linspace(n0, n1, num), np.linspace(m0, m1, num)
 
-# Extract the values along the line, using cubic interpolation
-zi = scipy.ndimage.map_coordinates(z, np.vstack((x,y)))
-zl = scipy.ndimage.map_coordinates(z, np.vstack((n,m)))
+# Extract the values along the lines, using cubic interpolation
+
+#bilinear/cubic interpolation (smooth graph)
+
+#First line (x,y)
+#firstline = []
+
+zxy1 = scipy.ndimage.map_coordinates(top_hybridPatterns[0], np.vstack((x,y)))
+zxy2 = scipy.ndimage.map_coordinates(top_hybridPatterns[1], np.vstack((x,y)))
+zxy3 = scipy.ndimage.map_coordinates(top_hybridPatterns[2], np.vstack((x,y)))
+zxy4 = scipy.ndimage.map_coordinates(top_hybridPatterns[3], np.vstack((x,y)))
+zxy5 = scipy.ndimage.map_coordinates(top_hybridPatterns[4], np.vstack((x,y)))
+
+#for i in range(5):
+    #firstline.append(firstline,scipy.ndimage.map_coordinates(top_hybridPatterns[i], np.vstack((x,y)))
+
+#firstline=firstline.reshape((5,256,256))
+
+#Second line (n,m)
+znm1 = scipy.ndimage.map_coordinates(top_hybridPatterns[0], np.vstack((n,m)))
+znm2 = scipy.ndimage.map_coordinates(top_hybridPatterns[1], np.vstack((n,m)))
+znm3 = scipy.ndimage.map_coordinates(top_hybridPatterns[2], np.vstack((n,m)))
+znm4 = scipy.ndimage.map_coordinates(top_hybridPatterns[3], np.vstack((n,m)))
+znm5 = scipy.ndimage.map_coordinates(top_hybridPatterns[4], np.vstack((n,m)))
+
 
 #Shows single window with all plots
-fig, axes = plt.subplots(nrows=3)
-axes[0].imshow(z)
-axes[0].plot([x0, x1], [y0, y1], 'w--')
-axes[0].plot([n0, n1], [m0, m1], 'w--')
-axes[0].axis('image')
+plt.figure(1)
+plt.imshow(z)
+plt.plot([x0, x1], [y0, y1], 'w--')
+plt.plot([n0, n1], [m0, m1], 'w--')
+plt.title('Maximum Pixel Values')
+plt.ylabel('y')
+plt.xlabel('x')
+plt.xlim(0,250)
+plt.ylim(0,250)
+plt.colorbar(label='[a.d.u.]')
 
-axes[0].set_title('Maximum Pixel Values')
-axes[0].set_ylabel('y')
-axes[0].set_xlabel('x')
-#FIND OUT HOW TO PUT COLOR BAR HERE
+fig, axes = plt.subplots(nrows=2)
 
-axes[1].plot(zi)
-axes[2].plot(zl)
+#plot (x,y) line scan
+
+#for i in firstline:
+    #axes[0].plot(i)
+
+axes[0].plot(zxy1)
+axes[0].plot(zxy2)
+axes[0].plot(zxy3)
+axes[0].plot(zxy4)
+axes[0].plot(zxy5)
+
+#(x,y) line scan threshold
+p0, q0 = 0, 135000 
+p1, q1 = 1000, 135000
+t1 = axes[0].plot([p0, p1], [q0, q1], 'k--', label = 'Saturation Threshold 135000 a.d.u.')
+axes[0].legend()
+
+axes[0].set_title('Pixel Values in Intense Patterns along y=133')
+axes[0].set_ylabel('Raw Pixel Value [a.d.u.]')
+axes[0].set_xlabel('x(fast changing dim.)')
+
+
+#plot (n,m) line scan
+axes[1].plot(znm1)
+axes[1].plot(znm2)
+axes[1].plot(znm3)
+axes[1].plot(znm4)
+axes[1].plot(znm5)
+
+#(n,m) line scan threshold
+j0, k0 = 0, 70000 
+j1, k1 = 1000, 70000
+t2 = axes[1].plot([j0, j1], [k0, k1], 'k--', label = 'Saturation Threshold 70000 a.d.u.')
+axes[1].legend()
+
+axes[1].set_title('Pixel Values in Intense Patterns along y=138')
+axes[1].set_ylabel('Raw Pixel Value [a.d.u.]')
+axes[1].set_xlabel('x(fast changing dim.)')
 
 plt.show()
 
-
-#Shows individual windows for each plot
-#plt.figure(1)
-#plt.plot(zi)
-
-#plt.figure(2)
-#plt.plot(zl)
-
-#plt.figure(3)
-#plt.imshow(z)
-#plt.plot([x0, x1], [y0, y1], 'w--')
-#plt.plot([n0, n1], [m0, m1], 'w--')
-#plt.title('Maximum Pixel Values')
-#plt.ylabel('y')
-#plt.xlabel('x')
-#plt.xlim(0,250)
-#plt.ylim(0,250)
-#plt.colorbar(label='[a.d.u.]')
-
-#plt.show()
 #Get this warning when I have several plots show instead of using subplots: /reg/neh/home/benedikt/local/lib/python2.7/site-packages/matplotlib-1.3.1-py2.7-linux-x86_64.egg/matplotlib/collections.py:548: FutureWarning: elementwise comparison failed; returning scalar instead, but in the future will perform elementwise comparison  if self._edgecolors == 'face':
+
 #-----------------------------------------------------------------------------------------------------------------
 #Determine saturation threshold for the 4 quadrants from the line scan patterns (Save Plots for visual inspection)
 #-----------------------------------------------------------------------------------------------------------------
