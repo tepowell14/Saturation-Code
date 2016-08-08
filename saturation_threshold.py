f3a64fe497e74669eec3eeeac99dfb5077c858ce
tpowell <tpowell@slac.stanford.edu>
2016-08-08 12:43:31 -0700

    Using front key for scattering pattern instead of hybrid pattern.

--- saturation_threshold.py
+++ saturation_threshold.py
@@ -17,7 +17,7 @@ args = parser.parse_args()
 
 with h5py.File(filename %args.run, 'r') as f:
     intensities = f['intensityMJUM2'][:]
-    Patterns = f['hybridPattern'][:]
+    Patterns = f['front'][:]
 
 #---------------------------------------------------------------------------------
 #Find and save 5 most intense hits (expected to be hits with highest # lit pixels)
@@ -39,14 +39,14 @@ for intensity in intensities:
 
 print max_intensities
 
-# keep track of max_intensities indices to find corresponding hybrid patterns
-top_hybridPatterns = []
+# keep track of max_intensities indices to find corresponding scattering patterns
+top_scatteringPatterns = []
 
 for i in max_intensities:
     dex = np.where(intensities==i)
-    top_hybridPatterns.append(Patterns[dex][0])
+    top_scatteringPatterns.append(Patterns[dex][0])
 
-#print top_hybridPatterns
+#print top_scatteringPatterns
 
 #------------------------------------------------------
 #Print information of interest (average/max intensity)
@@ -65,34 +65,34 @@ print "median = %.2e" % (np.median(maxintensities_arr))
 #---------------------------------------------------------------------------------------------------------
 #Getting maximum pixels values of patterns in a run (from top most intense hits in a run)
 
-top_hybridPatterns_arr = np.array(top_hybridPatterns)
+top_scatteringPatterns_arr = np.array(top_scatteringPatterns)
     
-patt1 = np.array(top_hybridPatterns[0])
-patt2 = np.array(top_hybridPatterns[1])
-patt3 = np.array(top_hybridPatterns[2])
-patt4 = np.array(top_hybridPatterns[3])
-patt5 = np.array(top_hybridPatterns[4])
+patt1 = np.array(top_scatteringPatterns[0])
+patt2 = np.array(top_scatteringPatterns[1])
+patt3 = np.array(top_scatteringPatterns[2])
+patt4 = np.array(top_scatteringPatterns[3])
+patt5 = np.array(top_scatteringPatterns[4])
 
-maxhybrid_arr = np.maximum(patt1, patt2)
-maxhybrid_arr = np.maximum(maxhybrid_arr, patt3)
-maxhybrid_arr = np.maximum(maxhybrid_arr, patt4)
-maxhybrid_arr = np.maximum(maxhybrid_arr, patt5)
+maxscattering_arr = np.maximum(patt1, patt2)
+maxscattering_arr = np.maximum(maxscattering_arr, patt3)
+maxscattering_arr = np.maximum(maxscattering_arr, patt4)
+maxscattering_arr = np.maximum(maxscattering_arr, patt5)
 
-max_hybrid = maxhybrid_arr.tolist()
+max_scattering = maxscattering_arr.tolist()
 
 #-------------------------------------------------------------------------
 #For the intense patterns plot the pixel values along two horizontal lines (Input the y= lines to scan across)
 #-------------------------------------------------------------------------
 #Set up the data
-z = max_hybrid
+z = max_scattering
 
 plt.figure(1)
 plt.imshow(z)
 plt.title('Maximum Pixel Values')
 plt.ylabel('y')
 plt.xlabel('x')
-plt.xlim(0,250)
-plt.ylim(0,250)
+plt.xlim(0,1000)
+plt.ylim(0,1000)
 plt.colorbar(label='[a.d.u.]')
 plt.show()
 
@@ -107,12 +107,12 @@ def plotting(thr_lines):
 
     #Extract the lines in pixel coordinates with num points
     x0, y0 = 0, line1 
-    x1, y1 = 250, line1
+    x1, y1 = 1000, line1
     num = 1000
     x, y = np.linspace(x0, x1, num), np.linspace(y0, y1, num)
 
     n0, m0 = 0, line2 
-    n1, m1 = 250, line2
+    n1, m1 = 1000, line2
     n, m = np.linspace(n0, n1, num), np.linspace(m0, m1, num)
 
     # Extract the values along the lines, using cubic interpolation
@@ -120,8 +120,8 @@ def plotting(thr_lines):
     firstline = []
     secondline = []
     for i in range (0,5):
-        firstline.append(scipy.ndimage.map_coordinates(top_hybridPatterns[i], np.vstack((x,y))))
-        secondline.append(scipy.ndimage.map_coordinates(top_hybridPatterns[i], np.vstack((n,m))))
+        firstline.append(scipy.ndimage.map_coordinates(top_scatteringPatterns[i], np.vstack((x,y))))
+        secondline.append(scipy.ndimage.map_coordinates(top_scatteringPatterns[i], np.vstack((n,m))))
 
     plt.figure()
     plt.imshow(z)
@@ -130,8 +130,8 @@ def plotting(thr_lines):
     plt.title('Maximum Pixel Values')
     plt.ylabel('y')
     plt.xlabel('x')
-    plt.xlim(0,250)
-    plt.ylim(0,250)
+    plt.xlim(0,1000)
+    plt.ylim(0,1000)
     plt.colorbar(label='[a.d.u.]')
 
     #plotting line scans
@@ -145,14 +145,24 @@ def plotting(thr_lines):
     if thr_lines == True:
         #(x,y) line scan threshold
         p0, q0 = 0, sat_thr1 
-        p1, q1 = 1000, sat_thr1
+        p1, q1 = 500, sat_thr1
         t1 = axes[0].plot([p0, p1], [q0, q1], 'k--')#, label = 'Saturation Threshold 135000 a.d.u.')
         #axes[0].legend()
 
+        r0, s0 = 500, sat_thr2 
+        r1, s1 = 1000, sat_thr2
+        t2 = axes[0].plot([r0, r1], [s0, s1], 'k--')#, label = 'Saturation Threshold 135000 a.d.u.')
+        #axes[0].legend()
+
         #(n,m) line scan threshold
-        j0, k0 = 0, sat_thr2 
-        j1, k1 = 1000, sat_thr2
-        t2 = axes[1].plot([j0, j1], [k0, k1], 'k--')
+        j0, k0 = 0, sat_thr3
+        j1, k1 = 500, sat_thr3
+        t3 = axes[1].plot([j0, j1], [k0, k1], 'k--')
+        #axes[1].legend()
+
+        l0, f0 = 500, sat_thr4
+        l1, f1 = 1000, sat_thr4
+        t4 = axes[1].plot([l0, l1], [f0, f1], 'k--')
         #axes[1].legend()
     
     axes[0].set_title('Pixel Values in Intense Patterns along y=%s' %line1)
@@ -164,6 +174,8 @@ def plotting(thr_lines):
     axes[1].set_ylabel('Raw Pixel Value [a.d.u.]')
     axes[1].set_xlabel('x(fast changing dim.)')
 
+    plt.savefig('/reg/d/psdm/amo/amol3416/scratch/tpowell/max_intensities/r%04d_thresholds.png' %args.run)
+
     plt.show()
 
 plotting(False)
@@ -174,9 +186,15 @@ plotting(False)
 #Determine saturation threshold for the 4 quadrants from the line scan patterns (Save Plots for visual inspection)
 #-----------------------------------------------------------------------------------------------------------------
 
-sat_thr1 = int(raw_input('Enter first saturation threshold: '))
+sat_thr1 = int(raw_input('Enter first quadrant threshold: '))
 
-sat_thr2 = int(raw_input('Enter second saturation threshold: '))
+sat_thr2 = int(raw_input('Enter second quadrant threshold: '))
+
+sat_thr3 = int(raw_input('Enter third quadrant threshold: '))
+
+sat_thr4 = int(raw_input('Enter fourth quadrant threshold: '))
+
+quad_thresholds = [sat_thr1, sat_thr2, sat_thr3, sat_thr4]
 
 #135000
 #70000
@@ -184,13 +202,14 @@ sat_thr2 = int(raw_input('Enter second saturation threshold: '))
 plotting(True)
 
 #---------------------------------------------------------------------------------------------------------
-#Save top hybrid patterns, max hybrid pattern & max intensities
+#Save top scattering patterns, max scattering pattern & max intensities
 #---------------------------------------------------------------------------------------------------------
 
 #Saving to file
 
 f = h5py.File('/reg/d/psdm/amo/amol3416/scratch/tpowell/max_intensities/r%04d_top.h5' %args.run, 'w')
-f.create_dataset('tophybridPatterns', data = top_hybridPatterns)
+f.create_dataset('topscatteringPatterns', data = top_scatteringPatterns)
 f.create_dataset('maxintensityMJUM2', data = max_intensities)
-f.create_dataset('maxhybridPattern', data = max_hybrid)
+f.create_dataset('maxscatteringPattern', data = max_scattering)
+f.create_dataset('saturationThresholds', data = quad_thresholds)
 f.close()
